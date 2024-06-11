b8767e11ef79e9414215b0a0c8ff64b1752d09a9
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-06-11 12:52:29 +0000

    feat: Add Proton Pass

--- build_files/base/install-stuff.sh
+++ build_files/base/install-stuff.sh
@@ -5,4 +5,9 @@ set -ouex pipefail
 wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm -O /tmp/ProtonMail-desktop-beta.rpm
 rpm-ostree install /tmp/ProtonMail-desktop-beta.rpm
 
+wget https://proton.me/download/PassDesktop/linux/x64/version.json -O - | jq '[.Releases[] | select(.CategoryName == "Stable")][0].File[] | select(.Identifier == ".rpm (Fedora/RHEL)")' > /tmp/protonpass.json
+wget -O /tmp/protonpass.rpm "$(jq -r '.Url' /tmp/protonpass.json)"
+echo "$(jq -r '.Sha512CheckSum' /tmp/protonpass.json) /tmp/protonpass.rpm" | sha512sum --check --status
+rpm-ostree install /tmp/protonpass.rpm
+
 . /tmp/build/install-1password.sh
